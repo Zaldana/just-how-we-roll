ff3dea732b9ceb0ba41f5aa7ea210ccd0c42a85f
Zaldana <rooben.zaldana@gmail.com>
2021-07-08 12:52:17 -0400

    working mode

--- main.js
+++ main.js
@@ -50,6 +50,8 @@ function rollD6() {
   d6mean.innerText = mean(sixes);
   const d6median = document.querySelector("#d6-rolls-median");
   d6median.innerText = median(sixes);
+  const d6mode = document.querySelector("#d6-rolls-mode");
+  d6mode.innerText = mode(sixes);
   
 
 
@@ -76,6 +78,8 @@ function rollDouble6() {
   dd6mean.innerText = mean(doubleSixes);
   const dd6median = document.querySelector("#double-d6-rolls-median");
   dd6median.innerText = median(doubleSixes);
+  const dd6mode = document.querySelector("#double-d6-rolls-mode");
+  dd6mode.innerText = mode(doubleSixes);
   
 
 }
@@ -97,7 +101,9 @@ function rollD12() {
   const d12mean = document.querySelector("#d12-rolls-mean");
   d12mean.innerText = mean(twelves);
   const d12median = document.querySelector("#d12-rolls-median");
-  d12median.innerText = median(twelves);
+  d12median.innerText = median(twelves)
+  const d12mode = document.querySelector("#d12-rolls-mode");
+  d12mode.innerText = mode(twelves);
 
 }
 
@@ -118,6 +124,8 @@ function rollD20() {
   d20mean.innerText = mean(twenties);
   const d20median = document.querySelector("#d20-rolls-median");
   d20median.innerText = median(twenties);
+  const d20mode = document.querySelector("#d20-rolls-mode");
+  d20mode.innerText = mode(twenties)
 
 }
 
@@ -216,5 +224,98 @@ function median(arr) {
   } 
 
   return output;
+
+}
+
+//Mode Function
+function mode(arr) {
+
+  //Scenario A  
+  //If the array only has 1 value....
+  if (arr.length === 1) {
+
+    //return that value as the mode
+    return arr[0];
+
+    //If the array is longer than 1
+  } if (arr.length > 1) {
+
+    //Sort the array to have all equal numbers next to each other
+    let arrSort = sortByNumber(arr);
+
+    //Starting values for variables
+    let mostCommoncount = 1;
+    let mostCommonindex = arr[0];
+    let newCount = 1;
+    let newIndex = arr[0];
+
+    //For loop starting at the second number
+    for (let i = 1; i < arrSort.length; i++) {
+
+      //Scenario B
+      //starting from the second digit if the previous number does not equal the current number...
+      if (arrSort[i - 1] !== arrSort[i]) {
+
+        //AND there is only 2 digits...
+        if (arrSort.length === 2) {
+
+          //Return no mode
+          return "No Mode"
+
+          //Scenario C
+          //If the previous number does not equal the current number
+          //AND If the current number appears more times than the amount of times the last number appeared... 
+        } if (newCount > mostCommoncount) {
+
+          //The current count is now the most common count AND...
+          mostCommoncount = newCount;
+
+          //The most common number is equal to the current number.
+          mostCommonindex = newIndex;
+
+        }
+
+        //Reset current number index and count
+        newCount = 0;
+        newIndex = arrSort[i];
+
+      }
+
+      //Scenario D
+      //starting from the second digit if the previous number equals the current number add 1 to the new count number.
+      newCount++
+
+    }
+
+    //Final Math
+    //If the new count is greater than the last number with the most common frequency...
+    if (newCount > mostCommoncount) {
+
+      //Return the new Index as the mode.
+      return newIndex;
+
+      //If the last number with the most common frequency is greater than the new count...
+    } if (newCount < mostCommoncount) {
+
+      //Return the most common index value
+      return mostCommonindex
+
+      //If more than one mode...
+    } if (newCount === mostCommoncount) {
+
+        if (newCount === 1 && mostCommoncount === 1) {
+
+          return "No Mode";
+
+        } else {
+
+      //Return the most common index value
+      return mostCommonindex + ' & ' + newIndex;
+
+    }
+
+  }
+
+}
 
 }
